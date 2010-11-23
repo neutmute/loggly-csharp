c108f9f87583c4ae47b3b40d137dc5e64c3123d6
Karl Seguin <karl@openmymind.net>
2010-11-23 08:13:52 +0800

    renabled facets..still need to add tests though

--- loggly-csharp/FacetQuery.cs
+++ loggly-csharp/FacetQuery.cs
@@ -1,45 +1,45 @@
-//using System;
-//using System.Collections.Generic;
-//
-//namespace Loggly
-//{
-//   public class FacetQuery
-//   {
-//      public string Query { get; set; }
-//      public DateTime? From { get; set; }
-//      public DateTime? Until { get; set; }
-//      public Order? OrderBy { get; set; }
-//      public int? NumberOfBuckets { get; set; }
-//      public string Gap { get; set; }
-//      public Fields? FieldsToFacet { get; set; }
-//
-//      public IDictionary<string, object> ToParameters()
-//      {
-//         return new Dictionary<string, object>
-//                {
-//                   { "q", Query },
-//                   { "buckets", NumberOfBuckets == null ? null : (int?)NumberOfBuckets.Value },
-//                   { "order", SerializeOrder(OrderBy) },
-//                   { "from", From == null ? null : From.Value.ToLogglyDateTime() },
-//                   { "until", Until == null ? null : Until.Value.ToLogglyDateTime() },
-//                   { "gap", Gap},
-//                   { "facetby", SerializeFields(FieldsToFacet)},
-//                };
-//      }
-//
-//      private static string SerializeOrder(Order? orderBy)
-//      {
-//         if (orderBy == Order.Ascending) { return "asc"; }
-//         if (orderBy == Order.Descending) { return "desc"; }
-//         return null;
-//      }
-//
-//      private static string SerializeFields(Fields? fieldsToFacet)
-//      {         
-//         if (fieldsToFacet  == Fields.Ip) { return "ip"; }
-//         if (fieldsToFacet  == Fields.InputName) { return "inputname"; }
-//         if (fieldsToFacet == Fields.Text) { return "text"; }
-//         return null;
-//      } 
-//   }
-//}
+using System;
+using System.Collections.Generic;
+
+namespace Loggly
+{
+   public class FacetQuery
+   {
+      public string Query { get; set; }
+      public DateTime? From { get; set; }
+      public DateTime? Until { get; set; }
+      public Order? OrderBy { get; set; }
+      public int? NumberOfBuckets { get; set; }
+      public string Gap { get; set; }
+      public Fields? FieldsToFacet { get; set; }
+
+      public IDictionary<string, object> ToParameters()
+      {
+         return new Dictionary<string, object>
+                {
+                   { "q", Query },
+                   { "buckets", NumberOfBuckets == null ? null : (int?)NumberOfBuckets.Value },
+                   { "order", SerializeOrder(OrderBy) },
+                   { "from", From == null ? null : From.Value.ToLogglyDateTime() },
+                   { "until", Until == null ? null : Until.Value.ToLogglyDateTime() },
+                   { "gap", Gap},
+                   { "facetby", SerializeFields(FieldsToFacet)},
+                };
+      }
+
+      private static string SerializeOrder(Order? orderBy)
+      {
+         if (orderBy == Order.Ascending) { return "asc"; }
+         if (orderBy == Order.Descending) { return "desc"; }
+         return null;
+      }
+
+      private static string SerializeFields(Fields? fieldsToFacet)
+      {         
+         if (fieldsToFacet  == Fields.Ip) { return "ip"; }
+         if (fieldsToFacet  == Fields.InputName) { return "inputname"; }
+         if (fieldsToFacet == Fields.Text) { return "text"; }
+         return null;
+      } 
+   }
+}
--- loggly-csharp/Responses/FacetResponse.cs
+++ loggly-csharp/Responses/FacetResponse.cs
@@ -1,33 +1,21 @@
-//using System.Collections.Generic;
-//using Newtonsoft.Json;
-//
-//namespace Loggly.Responses
-//{
-//   public class FacetResponse
-//   {
-//      private IDictionary<string, int> _data;
-//
-//      [JsonProperty("numFound")]
-//      public int TotalRecords { get; set; }
-//      [JsonProperty("numFound")]
-//      public int Start { get; set; }
-//      [JsonProperty("numFound")]
-//      public string Gap { get; set; }
-//      [JsonProperty("gmt_offset")]
-//      public string GmtOffset { get; set; }
-//      public Context Context { get; set; }      
-//      public IDictionary<string, int> Data
-//      {
-//         get { return _data ?? (_data = new Dictionary<string, int>(0)); }
-//      }
-//   }
-//
-//   public class Context
-//   {
-//      public string From { get; set; }
-//      public string Until { get; set; }
-//      public int Start { get; set; }
-//      public string Query { get; set; }
-//      public string Order { get; set; }
-//   }   
-//}
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Loggly.Responses
+{
+   public class FacetResponse
+   {
+      private IDictionary<string, int> _data;
+
+      [JsonProperty("numFound")]
+      public int TotalRecords { get; set; }      
+      public int Start { get; set; }      
+      public string Gap { get; set; }
+      [JsonProperty("gmt_offset")]
+      public string GmtOffset { get; set; }     
+      public IDictionary<string, int> Data
+      {
+         get { return _data ?? (_data = new Dictionary<string, int>(0)); }
+      }
+   }  
+}
