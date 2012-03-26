e9a59425083937b1389b0b71bd638dc091259d85
Karl Seguin <karl@openmymind.net>
2012-03-26 12:46:13 +0800

    re-introduced integration tests, added methods to ILogger interface, changed to markdown for readme

--- package.cmd
+++ package.cmd
@@ -3,7 +3,7 @@ if not exist Download\package mkdir Download\package
 if not exist Download\package\lib mkdir Download\package\lib
 if not exist Download\package\lib\net35 mkdir Download\package\lib\net35
 
-tools\ilmerge.exe /lib:loggly-csharp\bin\Release /internalize /ndebug /v2 /out:Download\Loggly.dll Loggly.dll Newtonsoft.Json.dll
+ilmerge.exe /lib:loggly-csharp\bin\Release /internalize /ndebug /v2 /out:Download\Loggly.dll Loggly.dll Newtonsoft.Json.dll
 
 copy LICENSE.txt Download
 
