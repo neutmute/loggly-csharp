e65e666565efe70206c73b01e0e3ae66b1be1f93
Karl Seguin <karl@openmymind.net>
2012-03-26 12:44:50 +0800

    re-introduced integration tests, added methods to ILogger interface, changed to markdown for readme

--- loggly-csharp/ILogger.cs
+++ loggly-csharp/ILogger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Loggly.Responses;
 
 namespace Loggly
@@ -8,7 +9,7 @@ namespace Loggly
       /// <summary>
       /// Synchronously logs a message
       /// </summary>
-      /// <param name="message">The message to log</param>     
+      /// <param name="message">The message to log</param>
       LogResponse LogSync(string message);
 
       /// <summary>
@@ -29,5 +30,16 @@ namespace Loggly
       /// Callback can be null which will give great performance, at the cost of not knowing if a failure occured.
       /// </remarks>
       void Log(string message, Action<LogResponse> callback);
+
+      void Log(string message, string category);
+      void Log(string message, string category, IDictionary<string, object> data);
+      void LogInfo(string message);
+      void LogInfo(string message, IDictionary<string, object> data);
+      void LogVerbose(string message);
+      void LogVerbose(string message, IDictionary<string, object> data);
+      void LogWarning(string message);
+      void LogWarning(string message, IDictionary<string, object> data);
+      void LogError(string message, Exception ex);
+      void LogError(string message, Exception ex, IDictionary<string, object> data);
    }
 }
