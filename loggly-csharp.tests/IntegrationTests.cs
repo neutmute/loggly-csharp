7eae02fcc0b03d2bed8bf25a447ccac5378d41be
Karl Seguin <karl@openmymind.net>
2012-03-26 12:46:20 +0800

    re-introduced integration tests, added methods to ILogger interface, changed to markdown for readme

---
+++ loggly-csharp.tests/IntegrationTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Threading;
+using Loggly.Responses;
+using NUnit.Framework;
+
+namespace Loggly.Tests
+{
+   /// <summary>
+   /// WARNING: some of these tests take long, because it will test that the log is actually indexed
+   /// </summary>
+   public class IntegrationTests
+   {
+      private ILogger _logger;
+
+      [SetUp]
+      public void SetUp()
+      {
+         LogglyConfiguration.Configure(c => c.AuthenticateWith(ConfigurationManager.AppSettings["IntegrationUser"], ConfigurationManager.AppSettings["IntegrationPassword"]));
+         _logger = new Logger(ConfigurationManager.AppSettings["IntegrationKey"]);
+      }
+
+      [Test, Category("Integration")]
+      public void SyncLogToPlainTextInput()
+      {
+         var randomString = GenerateRandomString(8);
+         _logger.LogSync(randomString);
+         var response = StartThread(randomString);
+         Assert.IsNotNull(response);
+         Assert.AreEqual(1, response.TotalRecords);
+         Assert.AreEqual(randomString, response.Results[0].Text);
+      }
+
+      [Test, Category("Integration")]
+      public void LogInfoToJsonInputAsync()
+      {
+         var randomString = GenerateRandomString(8);
+         _logger.LogInfo(randomString, new Dictionary<string, object> {{"key1", "value1"}, {"key2", "value2"}});
+         var response = StartJsonThread(randomString, "message");
+         Assert.IsNotNull(response);
+         Assert.AreEqual(1, response.TotalRecords);
+         Assert.AreEqual(randomString, response.Results[0].Json["message"]);
+         Assert.AreEqual("info", response.Results[0].Json["category"]);
+         Assert.AreEqual("value1", response.Results[0].Json["key1"]);
+         Assert.AreEqual("value2", response.Results[0].Json["key2"]);
+      }
+
+      [Test, Category("Integration")]
+      public void LogErrorToJsonInputAsync()
+      {
+         var randomString = GenerateRandomString(8);
+         _logger.LogError(randomString, new InvalidOperationException("oops" + randomString + " something went wrong"));
+         var response = StartJsonThread("oops" + randomString, "exception");
+         Assert.IsNotNull(response);
+         Assert.AreEqual(1, response.TotalRecords);
+         Assert.AreEqual(randomString, response.Results[0].Json["message"]);
+         Assert.AreEqual("error", response.Results[0].Json["category"]);
+         Assert.AreEqual("System.InvalidOperationException: oops" + randomString + " something went wrong", response.Results[0].Json["exception"]);
+      }
+
+      [Test, Category("Integration")]
+      public void LogVerboseToJsonInputAsync()
+      {
+         var randomString = GenerateRandomString(8);
+         _logger.LogVerbose(randomString);
+
+         var response = StartJsonThread(randomString, "message");
+         Assert.IsNotNull(response);
+         Assert.AreEqual(1, response.TotalRecords);
+         Assert.AreEqual(randomString, response.Results[0].Json["message"]);
+         Assert.AreEqual("verbose", response.Results[0].Json["category"]);
+      }
+
+
+      [Test, Category("Integration")]
+      public void LogWarningToJsonInputAsync()
+      {
+         var randomString = GenerateRandomString(8);
+         _logger.LogWarning(randomString);
+         var response = StartJsonThread(randomString, "message");
+         Assert.IsNotNull(response);
+         Assert.AreEqual(1, response.TotalRecords);
+         Assert.AreEqual(randomString, response.Results[0].Json["message"]);
+         Assert.AreEqual("warning", response.Results[0].Json["category"]);
+      }
+
+      private static SearchResponse StartThread(string randomString)
+      {
+         var signal = new AutoResetEvent(false);
+         SearchResponse response = null;
+         new Thread(() =>
+         {
+            while (true)
+            {
+               Thread.Sleep(3000);
+               response = new Searcher("csharptests").Search(randomString);
+               if (response.TotalRecords > 0) { break; }
+            }
+            signal.Set();
+         }).Start();
+         signal.WaitOne(50000); // wait till loggly index the new event (if it didn't after 50 seconds it is broken)
+         return response;
+      }
+
+      private static SearchJsonResponse StartJsonThread(string randomString, string property)
+      {
+         var signal = new AutoResetEvent(false);
+         SearchJsonResponse response = null;
+         new Thread(() =>
+         {
+            while (true)
+            {
+               Thread.Sleep(3000);
+               response = new Searcher("csharptests").SearchJson(property, randomString);
+               if (response.TotalRecords > 0) { break; }
+            }
+            signal.Set();
+         }).Start();
+         signal.WaitOne(50000); // wait till loggly index the new event (if it didn't after 50 seconds it is broken)
+         return response;
+      }
+
+      private static string GenerateRandomString(int size)
+      {
+         var builder = new StringBuilder();
+         var random = new Random();
+         for (var i = 0; i < size; ++i)
+         {
+            var ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26*random.NextDouble() + 65)));
+            builder.Append(ch);
+         }
+
+         return builder.ToString().ToLower();
+      }
+   }
+}
---
+++ readme.md
@@ -0,0 +1,58 @@
+# Loggly .NET Driver
+
+This is a .NET driver for [loggly.com](http://loggly.com).
+
+## Logging Events
+
+**Please note that as of the 2.0 release, async logging is the default. To log synchronously, use the corresponding `Sync` methods (`Log` vs `LogSync`)**
+
+Create a new `Logger` with your input key:
+
+	var logger = new Logger("my-long-key-that-i-got-when-setting-up-my-http-input");
+
+For JSON logging you can use `LogInfo`, `LogVerbose`, `LogWarning`, `LogError` methods that will create json objects with properties like category, message, exception (if applicable), extra data that you provide.
+
+Use either a synchronous or asynchronous `Log` method.
+
+
+## Searching Events
+
+First, setup the username/password you want to connect with:
+
+	LogglyConfiguration.Configure(c => c.AuthenticateWith("username", "password"));
+
+Next, create a searcher with your domain:
+
+	var searcher = new Searcher("mydomain");
+
+Finally, use the various `Search` methods.
+
+For JSON search you can use `SearchJson` methods.
+
+Note that searching happens synchronously.
+
+
+## Facets
+
+First, setup the username/password you want to connect with:
+
+	LogglyConfiguration.Configure(c => c.AuthenticateWith("username", "password"));
+
+Next, create a facet with your domain:
+
+	var facet = new Facet("mydomain");
+
+Finally, use the various `GetDate`, `GetIp` and `GetInput* methods.
+
+Getting facts is always synchronous
+
+
+## Integration Tests
+
+To run the integration tests, you'll need to place a `config.user` file in the test's debug folder (assuming you are running tests in debug). The file should look something like:
+
+	<appSettings>
+		<add key="IntegrationKey" value="YOUR KEY"></add>
+		<add key="IntegrationUser" value="YOUR USERNAME"></add>
+		<add key="IntegrationPassword" value="YOUR PASSWORD"></add>
+	</appSettings>
