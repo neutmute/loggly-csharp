b4a25f49eddb147a63570f29edc7ebdc80ec0906
neutmute <neutmute@neutmute.nm>
2014-10-22 22:41:19 +1100

    nuget tweaks

--- SolutionItems/Properties/AssemblyInfo.cs
+++ SolutionItems/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Security;
 [assembly: AssemblyCompany("Karl Seguin")]
 [assembly: AssemblyCopyright("Copyright © Karl Seguin 2010")]
 [assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyInformationalVersion("3.5.0-alpha")]
+[assembly: AssemblyInformationalVersion("3.5.0-alpha-v2")]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: AssemblyProduct("loggly-csharp")]
 
--- source/Loggly.Config/Properties/AssemblyInfoUnique.cs
+++ source/Loggly.Config/Properties/AssemblyInfoUnique.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 using System.Security;
 
 [assembly: AssemblyTitle("Loggly.Config")]
-[assembly: AssemblyDescription("Configuration classes for loggly-csharp. Settings are read from app.config or can be programmatically injected at runtime. See readme.md for documentation")]
+[assembly: AssemblyDescription("Configuration classes for loggly clients. Settings are read from app.config or can be programmatically injected at runtime. See readme.md for documentation")]
