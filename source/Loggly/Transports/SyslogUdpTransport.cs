44e5828d2c09a6a9fefa96a433f02e8d012becba
neutmute <neutmute.email>
2014-10-24 20:07:29 +1100

    tidy up

--- source/Loggly/Transports/SyslogUdpTransport.cs
+++ source/Loggly/Transports/SyslogUdpTransport.cs
@@ -6,26 +6,6 @@ using Loggly.Config;
 
 namespace Loggly.Transports.Syslog
 {
-
-
-    /// <summary>
-    /// Exposes the Active propery of UdpClient
-    /// </summary>
-    public class UdpClientEx : UdpClient
-    {
-        public UdpClientEx() : base() { }
-        public UdpClientEx(IPEndPoint ipe) : base (ipe) { }
-        ~UdpClientEx()
-        {
-            if (this.Active) this.Close();
-        }
-
-        public bool IsActive
-        {
-            get {  return this.Active ; }
-        }
-    }
-
     internal class SyslogUdpTransport : SyslogTransportBase
     {
         private readonly UdpClientEx _udpClient;
