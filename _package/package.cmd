bc891364b4e2430d1085f629d50b784ea8aae7bf
neutmute <neutmute.email>
2014-10-26 16:47:20 +1100

    nuspec

--- _package/package.cmd
+++ _package/package.cmd
@@ -1,2 +1,3 @@
+del /q *.nupkg
 ..\.nuget\nuget.exe pack ..\source\Loggly.Config\Loggly.Config.csproj -IncludeReferencedProjects -Prop Configuration=Release
 ..\.nuget\nuget.exe pack ..\source\Loggly\Loggly.csproj -IncludeReferencedProjects -Prop Configuration=Release
