39f8ecc11dc606780d7557d8e9a3c55dba9912a5
neutmute <neutmute@neutmute.nm>
2014-11-03 22:47:43 +1100

    packaging

--- _package/build.cmd
+++ _package/build.cmd
@@ -1,2 +1,2 @@
 msbuild ..\loggly.sln /p:configuration=debug  /t:clean,build
-rem  msbuild ..\loggly.sln /p:configuration=release /t:clean,build
+msbuild ..\loggly.sln /p:configuration=release /t:clean,build
