812ce9bcaae8429d8665e1c9d1e931de95d81902
neutmute <neutmute@neutmute.nm>
2014-11-06 10:23:12 +1100

    nuspec

--- SolutionItems/Properties/AssemblyInfo.cs
+++ SolutionItems/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security;
 
-[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.5.0.1")]
 [assembly: AllowPartiallyTrustedCallers]
 [assembly: AssemblyProduct("loggly-csharp")]
 [assembly: InternalsVisibleTo("Loggly.Tests")]
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
-    [assembly: AssemblyInformationalVersion("4.5.0-alpha-v1")]
+    [assembly: AssemblyInformationalVersion("4.5.0.2-alpha-v1")]
 #else
     [assembly: AssemblyConfiguration("Release")]
-    [assembly: AssemblyInformationalVersion("4.5.0")]
+    //[assembly: AssemblyInformationalVersion("4.5.0")]
 #endif
 
