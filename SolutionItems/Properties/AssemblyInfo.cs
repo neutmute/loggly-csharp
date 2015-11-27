6a4769b4c09359bfe295677b5ccca16fa9b4febf
neutmute <neutmute@users.noreply.github.com>
2015-11-27 15:17:50 +1100

    try to fix myget build
    D:\temp\d5a8cbb\SolutionItems\Properties\AssemblyInfo.cs(14,52): error CS1040: Preprocessor directives must appear as the first non-whitespace character on a line [D:\temp\d5a8cbb\source\loggly.Config\Loggly.Config.csproj]

--- SolutionItems/Properties/AssemblyInfo.cs
+++ SolutionItems/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security;
 
-[assembly: AssemblyVersion("4.5.1.0")]
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyProduct("loggly-csharp")]
-[assembly: InternalsVisibleTo("Loggly.Tests")]
 #if DEBUG
     [assembly: AssemblyConfiguration("Debug")]
-    [assembly: AssemblyInformationalVersion("4.5.1.0-beta1")]    // trigger pre release package
 #else
     [assembly: AssemblyConfiguration("Release")]
 #endif
+
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyVersion("4.5.1.0")]
+[assembly: AssemblyProduct("loggly-csharp")]
+[assembly: InternalsVisibleTo("Loggly.Tests")]
 
