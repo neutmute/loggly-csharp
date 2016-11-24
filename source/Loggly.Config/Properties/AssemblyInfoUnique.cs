1e1c4f356b27b742795f1e58f78073de80a24718
Jose Carvalho <jose.pereira@faroeurope.com>
2016-11-24 11:04:36 +0000

    A new PR was added to the solution ("LogglyCsharp.snk") and the projects were signed.

--- source/Loggly.Config/Properties/AssemblyInfoUnique.cs
+++ source/Loggly.Config/Properties/AssemblyInfoUnique.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Loggly.Config")]
 [assembly: AssemblyDescription("Configuration classes for loggly clients. Settings are read from app.config or can be programmatically injected at runtime. See readme.md for documentation")]
--- source/Loggly/Properties/AssemblyInfoUnique.cs
+++ source/Loggly/Properties/AssemblyInfoUnique.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("loggly-csharp")]
 [assembly: AssemblyDescription("A .NET client for loggly. Supporting Https, Syslog UDP and encrypted Syslog TCP transports.")]
