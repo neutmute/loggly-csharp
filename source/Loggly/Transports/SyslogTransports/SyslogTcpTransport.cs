ac691f6c48af9d39b8cb9784e411899a16339b68
Niklas Arbin <niklas.arbin@bookbeat.com>
2018-08-24 11:04:01 +0200

    * Move connection initialization into the try catch block
    * Do not create a new tcp connection for every Send and use a semaphore to guarantee single thread access to Send()

--- source/Loggly/Transports/SyslogTransports/SyslogTcpTransport.cs
+++ source/Loggly/Transports/SyslogTransports/SyslogTcpTransport.cs
@@ -1,13 +1,19 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Security.Authentication;
+using System.Threading;
 using System.Threading.Tasks;
 using Loggly.Config;
 
 namespace Loggly.Transports.Syslog
 {
-    class SyslogTcpTransport : SyslogTransportBase
+    class SyslogTcpTransport : SyslogTransportBase, IDisposable
     {
+        private Stream _networkStream;
+        private TcpClient _tcpClient;
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1);
+
         protected string Hostname
         {
             get { return LogglyConfig.Instance.Transport.EndpointHostname; }
@@ -17,18 +23,25 @@ namespace Loggly.Transports.Syslog
         {
             return Task.FromResult<Stream>(client.GetStream());
         }
+        
 
         protected override async Task Send(SyslogMessage syslogMessage)
         {
-            var client = new TcpClient();
-            client.ConnectAsync(Hostname, LogglyConfig.Instance.Transport.EndpointPort).Wait();
-
+            await _semaphore.WaitAsync();
+            
             try
             {
+                if (_networkStream == null)
+                {
+                    _tcpClient = new TcpClient();
+                    await _tcpClient.ConnectAsync(Hostname, LogglyConfig.Instance.Transport.EndpointPort).ConfigureAwait(false);
+                    _networkStream = await GetNetworkStream(_tcpClient).ConfigureAwait(false);
+                }
+
                 byte[] messageBytes = syslogMessage.GetBytes();
-                var networkStream = await GetNetworkStream(client).ConfigureAwait(false);
-                await networkStream.WriteAsync(messageBytes, 0, messageBytes.Length).ConfigureAwait(false);
-                await networkStream.FlushAsync().ConfigureAwait(false);
+                
+                await _networkStream.WriteAsync(messageBytes, 0, messageBytes.Length).ConfigureAwait(false);
+                await _networkStream.FlushAsync().ConfigureAwait(false);
             }
             catch (AuthenticationException e)
             {
@@ -36,13 +49,19 @@ namespace Loggly.Transports.Syslog
             }
             finally
             {
-#if NET_STANDARD
-                client.Dispose();
-#else
-                client.Close();
-#endif
+                _semaphore.Release();
             }
 
+        }
+
+        public void Dispose()
+        {
+#if NET_STANDARD
+            _tcpClient?.Dispose();
+#else
+            _tcpClient?.Close();
+#endif
+            _semaphore?.Dispose();
         }
     }
 }
