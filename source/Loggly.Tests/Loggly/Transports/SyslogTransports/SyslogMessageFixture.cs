40133ea1fd9cd2924ac9b51891d2a1c5a823dea3
Niklas Arbin <niklas.arbin@bookbeat.com>
2018-08-24 16:15:35 +0200

    add terminator to test

--- source/Loggly.Tests/Loggly/Transports/SyslogTransports/SyslogMessageFixture.cs
+++ source/Loggly.Tests/Loggly/Transports/SyslogTransports/SyslogMessageFixture.cs
@@ -33,7 +33,7 @@ namespace Loggly.Tests.Loggly.Transports.SyslogTransports
 
             var messageString = syslog.GetMessageAsString();
             Console.WriteLine(messageString);
-                 Assert.AreEqual("<34>1 2003-10-11T22:14:15.003000+00:00 server1.com sudo 900 31 [8bf8cc10-4140-4c3e-a2b4-e6f5324f1aea@41058]", messageString);
+                 Assert.AreEqual("<34>1 2003-10-11T22:14:15.003000+00:00 server1.com sudo 900 31 [8bf8cc10-4140-4c3e-a2b4-e6f5324f1aea@41058]\n", messageString);
 //loggly example Assert.AreEqual("<34>1 2003-10-11T22:14:15.003Z server1.com sudo - - [8bf8cc10-4140-4c3e-a2b4-e6f5324f1aea@41058]", messageString);
         }
     }
