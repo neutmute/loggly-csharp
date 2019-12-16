b2d3bf481de5477d942311898a609e5e57b4ceb6
Miguel Alho <miguel.alho@faroeurope.com>
2019-12-16 13:59:19 +0000

    support ForwardedForIp property in transport object
    There is a scenario where the loggly client is being used on desktop applications, and therefore the client's IP is being sent to loggly and indexed as clientHost. In some cases, this IP can be considered PII and undesired. By creating a new property on the transport object (ForwardedForIp), the requestes made to loggly can contain a new IP (or value) to be sent and override the client's IP.

--- source/Loggly.Config/TransportConfiguration.cs
+++ source/Loggly.Config/TransportConfiguration.cs
@@ -9,10 +9,12 @@
         public int EndpointPort { get; set; }
 
 		public bool IsOmitTimestamp { get; set; }
+		
+        public string ForwardedForIp { get; set; }
 
         public override string ToString()
         {
-			return string.Format("LogTransport={0}, EndpointHostname={1}, EndpointPort={2}, IsOmitTimestamp={3}", LogTransport, EndpointHostname, EndpointPort, IsOmitTimestamp);
+			return string.Format("LogTransport={0}, EndpointHostname={1}, EndpointPort={2}, IsOmitTimestamp={3}, ForwardedForIp={4}", LogTransport, EndpointHostname, EndpointPort, IsOmitTimestamp, ForwardedForIp);
         }
     }
 }
