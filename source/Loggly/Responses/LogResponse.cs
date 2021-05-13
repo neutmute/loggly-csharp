ef95f5a30d177618848830514eb1d5983033ddb4
Rolf Kristensen <sweaty1@hotmail.com>
2021-05-13 10:41:00 +0200

    LogResponse - Skip string.Format and allocation when succes

--- source/Loggly/Responses/LogResponse.cs
+++ source/Loggly/Responses/LogResponse.cs
@@ -4,21 +4,24 @@ namespace Loggly
 {
     public enum ResponseCode
     {
-        Unknown =0,
+        Unknown = 0,
         Success,
         Error,
         AssumedSuccess,
         SendDisabled
     }
-   public class LogResponse
-   {
-       public ResponseCode Code { get; set; }
+    public class LogResponse
+    {
+        public ResponseCode Code { get; set; }
 
-      public string Message { get; set; }
+        public string Message { get; set; }
 
-      public override string ToString()
-      {
-          return string.Format("Success={0}, Code={1}", Code, Message);
-      }
-   }
+        public override string ToString()
+        {
+            if (Code == ResponseCode.Success && string.IsNullOrEmpty(Message))
+                return "Success=Success, Code=";
+            else
+                return string.Format("Success={0}, Code={1}", Code, Message);
+        }
+    }
 }
